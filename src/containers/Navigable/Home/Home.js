fc7655cbe85938f8aee3b6760f9dbc3ed3275dbd
jodyrutter <jodyrutter@mail.usf.edu>
2019-10-11 19:48:53 -0400

    Buttons for pictures and camera added
    > Created a button script for uploading a picture from gallery
    > Created a button script for taking a picture

--- src/containers/Navigable/Home/Home.js
+++ src/containers/Navigable/Home/Home.js
@@ -1,22 +1,51 @@
 import React, {Component} from 'react';
+import Dimensions from 'Dimensions';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import {
     StyleSheet,
     Text,
     View,
     Image,
+    Button,
     ImageBackground,
     KeyboardAvoidingView,
     TouchableOpacity,
     TextInput,
   } from 'react-native';
+  import ButtonUploadPic from "./ButtonUploadPic";
+  import ButtonTakePic from "./ButtonTakePic";
 export default class Home extends React.Component {
   render(){
     return(
       <KeyboardAwareScrollView contentContainerStyle={{flexGrow: 1}}>
-          <Text>Home</Text>
+        <View style={styles.container}>
+          <Image style={styles.Image}
+            source={require('../../../assets/images/logo.png')}/>
+            <Text style={styles.Name}>FIRST NAME</Text>
+          <ButtonUploadPic style={styles.ButtonUploadPic}/>
+          <ButtonTakePic style={styles.ButtonTakePic}/>
+        </View>
       </KeyboardAwareScrollView>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.05,
+    top: 50,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  Image: {
+    width: 200,
+    height: 200,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  Name: {
+    top: 150,
+    fontSize: 16,
+    justifyContent: 'center'
+  },
+})
 
