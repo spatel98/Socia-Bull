941fc0d8c7f32a8d11588500552888c7365bf689
jodyrutter <jodyrutter@mail.usf.edu>
2019-10-11 23:47:23 -0400

    Got Photo Select and Camera Working
    > App now takes pictures and updates a box containing an image
    > App now allows user to select a photo and update the box containing an image
    > Removed former scripts for take pic and upload pic

--- src/containers/Navigable/Home/Home.js
+++ src/containers/Navigable/Home/Home.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import Dimensions from 'Dimensions';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import ImagePicker from 'react-native-image-picker';
 import {
+    Platform,
     StyleSheet,
     Text,
     View,
@@ -12,18 +14,59 @@ import {
     TouchableOpacity,
     TextInput,
   } from 'react-native';
-  import ButtonUploadPic from "./ButtonUploadPic";
-  import ButtonTakePic from "./ButtonTakePic";
-export default class Home extends React.Component {
+  import logo from "../../../assets/images/logo.png"
+  const options={
+    title: 'Upload an Image',
+    takePhotoButtonTitle: 'Take a photo',
+    chooseFromLibraryButtonTitle: 'Choose a photo',
+  }
+export default class Home extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state={
+      avatarSource: null,
+      pic:null
+    }
+  }
+  state = {
+    photo: logo,
+  }
+  handleChoosePhoto = () => {
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+  
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('Image Picker Error: ', response.error);
+      }
+  
+      else {
+        let source = { uri: response.uri };
+  
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+  
+        this.setState({
+          avatarSource: source,
+          pic:response.data
+        });
+      }
+    });
+  }
   render(){
+    const { photo } = this.state
     return(
       <KeyboardAwareScrollView contentContainerStyle={{flexGrow: 1}}>
         <View style={styles.container}>
           <Image style={styles.Image}
-            source={require('../../../assets/images/logo.png')}/>
+            source={this.state.avatarSource}/>
+            <TouchableOpacity style = {styles.SelectImgButton}
+              onPress={this.handleChoosePhoto}>
+              <Text style={styles.SelectImage}>Select Image</Text>
+            </TouchableOpacity>
             <Text style={styles.Name}>FIRST NAME</Text>
-          <ButtonUploadPic style={styles.ButtonUploadPic}/>
-          <ButtonTakePic style={styles.ButtonTakePic}/>
         </View>
       </KeyboardAwareScrollView>
     );
@@ -36,11 +79,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+  SelectImgButton: {
+    backgroundColor:'grey',
+    margin:10,
+    padding:10,
+  },
   Image: {
     width: 200,
     height: 200,
     borderWidth: 1,
     borderColor: 'black',
+  },
+  SelectImage: {
+    color:"#fff"
   },
   Name: {
     top: 150,
