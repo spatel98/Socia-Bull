91b68cbbc55a2baff7985f66111a1688f738107e
TacoSupremes <zscharoun@gmail.com>
2019-10-31 15:31:56 -0400

    chats page shows friend requests

--- src/containers/Navigable/Chats/Chats.js
+++ src/containers/Navigable/Chats/Chats.js
@@ -18,10 +18,9 @@ import {
 
 import _ from 'lodash'
 import {
-ListItem, Icon
+ListItem, Icon, Badge, Button
 } from 'react-native-elements';
 import Modal from 'react-native-modal';
-import { Button } from 'react-native-paper';
 import firebase from 'react-native-firebase'
 import firebaseSDK from '../../../config/firebaseSDK';
 
@@ -94,28 +93,34 @@ export default class Chats extends React.Component {
     friendText: "Enter Friend's Username",
     firstName: '',
     lastName: '',
-    matches: [],
-    users: [],
-    ids: []
+    matches: [], //ids in array
+    users: [], // actual user profiles
+    ids: [], // ids in users
+    requests: [], // request ids in array
+    requestsUsers: [],
+    rids: [],
+    idRequesting: '',
+    requesting: false
     };
 
-    console.log(this.state.firstName)
+   // console.log(this.state.firstName)
   };
 
   componentDidMount() {
-    this.fetch();
+   () => this.fetch();
  }
   
   keyExtractor = (item, index) => index.toString()
 
   fetch = () =>{
-  firebase.firestore().collection('users').doc(firebaseSDK.shared.uid).onSnapshot((doc)=>{
+  firebase.firestore().collection('users').doc(firebaseSDK.shared.uid).onSnapshot((doc) =>{
        if(doc.exists)
        {
         this.setState({matches: _.toArray(doc.data().matches)})
+        this.setState({requests: _.toArray(doc.data().requests)})
+      
        }
       
-       
       }).bind(this)
 
       this.state.matches.forEach( val => {
@@ -134,11 +139,23 @@ export default class Chats extends React.Component {
       })
       });
 
-      this.setState({isFetching: false})
+      this.state.requests.forEach( val => {
+        firebase.firestore().collection('users').doc(val).onSnapshot((doc)=>{
+ 
+        if(doc.exists)
+        {
+          if(!(this.state.rids.includes(val))){
+          
+         temp = {name: doc.data().firstName + ' ' + doc.data().lastName, email: val, id: val}
+         this.state.requestsUsers.push(temp)
+         this.state.rids.push(val)
+         }
+       }
+             
+       }).bind(this);
+       });
 
-      
-
-      
+      this.setState({isFetching: false})   
   };
 
   onRefresh = () => {
@@ -146,10 +163,44 @@ export default class Chats extends React.Component {
   }
 
   sendRequest = () => {
-    this.state.friendText = "Friend Request Sent";
+
+    if(!this.state.search.includes("@mail.usf.edu"))
+    {
+      this.state.friendText = "Please enter a valid USF Email"
+      return;
+    }
+
+
+    temp =''
+
+    // gets user with email
+
+    firebase.firestore().collection('users').where("email", "==", this.state.search)
+    .get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+          // doc.data() is never undefined for query doc snapshots
+        //    if(doc.exists)
+              this.setState({idRequesting: doc.id})
+              temp = doc.id
+      });
+
+      this.state.friendText = "Friend Request Sent";
+     
+      this.setAddFriendVisible(false)
+  })
+  .catch(function(error) {
+      console.log("Error getting documents: ", error);
+     
+  });
+
+  // set command that is supposed to update array
+ 
+  firebase.firestore().collection('users').doc(this.state.idRequesting).set({
+    requests: firebase.firestore.FieldValue.arrayUnion(this.state.idRequesting),
+    tests: 'epic'
+  })
+
   }
-
-
   setAddFriendVisible(t) {
 
     this.setState({addFriendVisible: t});
@@ -157,9 +208,6 @@ export default class Chats extends React.Component {
     if(!t)
     {
       this.setState({search: ''})
-    }
-    else
-    {
       this.state.friendText = "Enter Friend's Username"
     }
   }
@@ -169,6 +217,29 @@ export default class Chats extends React.Component {
   };
 
    addFriend = (props) => {
+
+    if(this.state.requestsUsers.length > 0)
+    return (
+      <ListItem
+      title={"Add Friend"}
+      
+      leftElement={
+        <Icon name='plus-circle' color={'#36485f'} size={50} type ='material-community'/>
+    
+      }
+
+      rightElement={
+        <Badge value= {this.state.requestsUsers.length}/>
+      }
+      onPress={()=>{
+        this.setAddFriendVisible(true)
+      }}
+
+      bottomDivider
+      chevron
+      ></ListItem>
+    );
+
     return (
       <ListItem
       title={"Add Friend"}
@@ -180,6 +251,7 @@ export default class Chats extends React.Component {
       onPress={()=>{
         this.setAddFriendVisible(true)
       }}
+
       bottomDivider
       chevron
       ></ListItem>
@@ -187,8 +259,6 @@ export default class Chats extends React.Component {
   }
 
   renderItem = ({ item }) => (
-
-   
     <ListItem
       title={item.name}
       subtitle={item.email}
@@ -210,12 +280,40 @@ export default class Chats extends React.Component {
     />
   )
 
+  renderRequests = ({ item }) => (
+    <ListItem
+      title={item.name}
+      subtitle={item.email}
+     // leftElement={
+      //  <Image
+       // style={styles.imagestyle}
+        //resizeMode="cover"
+        //source={{uri: item.photo}}
+        ///>
+      //}
+      rightElement={
+        <View>
+          <Icon name='minus-circle' color={'#36485f'} size={50} type ='material-community'/>
+          <Icon name='plus-circle' color={'#36485f'} size={50} type ='material-community'/>
+
+          </View>
+        
+      }
+  
+      bottomDivider
+      chevron
+      
+    />
+  )
+
 
   render(){
     return(
       <View>
         <Text style={styles.titlestyle}>
-         Chats
+         {this.state.idRequesting},
+         {this.state.requestsUsers},
+         {this.state.requests}
         </Text>
           <FlatList
           title = "Chats"
@@ -223,7 +321,7 @@ export default class Chats extends React.Component {
           data={this.state.users}
           refreshing = {this.state.isFetching}
           renderItem={this.renderItem}
-          onRefresh={this.onRefresh}
+          onRefresh={()=>this.onRefresh()}
           ListHeaderComponent={this.addFriend}
           />
         <Modal
@@ -234,6 +332,7 @@ export default class Chats extends React.Component {
           animationInTiming={400}
           animationOutTiming={400}
           > 
+
               <View style={styles.modalStyle}>
               <Text style= {styles.textstyle}>{this.state.friendText}</Text>
               <TextInput 
@@ -242,14 +341,26 @@ export default class Chats extends React.Component {
               onChangeText={text => this.updateSearch(text)}
               value={this.state.search}
               multiline={false}
-              onSubmitEditing={() => this.sendRequest()}
-              rightElement={
-                <Button onPress={this.sendRequest()}>
-
-                </Button>
-
-              }
+              onSubmitEditing={()=>this.setState({requesting: true},() => this.sendRequest())}
               />
+                <Button onPress={()=>this.setState({requesting: true},()=>this.sendRequest())}
+                 loading={this.state.requesting}></Button>
+           
+              
+              <FlatList
+          title = "Requests"
+          keyExtractor = {this.keyExtractor}
+          data={this.state.requestsUsers}
+         // refreshing = {this.state.isFetching}
+          renderItem={this.renderRequests}
+          //onRefresh={this.onRefresh}
+          //ListHeaderComponent={this.addFriend}
+          ListEmptyComponent={
+          <Text>No Pending Friend Requests
+          </Text>}
+          />
+
+           
               </View>
         </Modal>
       </View>
