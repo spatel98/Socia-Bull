38f5897f626c8a0004bc24d03c02aea4e5d36f09
jodyrutter <jodyrutter@mail.usf.edu>
2019-11-06 09:45:58 -0500

    Upped Quality
    > Added wallpapers to the application to make it standout with much higher quality.
    > Added a logout button to the home screen.
    > Changed Loading such that it sends the user to the login screen when they sign out.
    > Added a delete account page to settings, but the delete button still doesn't fully work.
    > Fixed everything except the delete account button in the settings page.

--- src/containers/Auth/SignUpScreen.js
+++ src/containers/Auth/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Button, ImageBackground } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import SignUpForm from './SignUpForm'
@@ -8,20 +8,28 @@ export default class SignUp extends React.Component {
   render() {
     return (
       <KeyboardAwareScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={styles.container2}>
+        <ImageBackground source={require('../../assets/images/background-5.png')} style={{width: '100%',height:'100%' }}>
         <View style={styles.container}>
           <SignUpForm navigation={this.props.navigation}></SignUpForm>
+        </View>
+        </ImageBackground>
         </View>
       </KeyboardAwareScrollView>
     )
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#36485f',
     paddingLeft: 60,
     paddingRight: 60,
+  },
+  container2: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 0,
+    paddingRight: 0,
   },
 })
